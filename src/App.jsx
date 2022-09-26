47a7f8946cc741944e424d169e0ff5153f367dd2
jonasCr <jonascr.dev@gmail.com>
2022-09-26 16:14:59 +0200

    feat: project structure + basic template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='block md:flex'>
+      <div className="bg-orange-500 flex-none">
+        <h1>Me</h1>
+      </div>
+      <div className='bg-orange-50 flex-auto'>
+        <h1>Mi career</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
