5e451e208c3bda17254e9e236b4076a15e259436
jonasCr <jonascr.dev@gmail.com>
2022-09-29 15:57:12 +0200

    refactor: moving image to assets folders

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import useScrollSpy from '../hooks/UseScrollSpy';
 
-const Navbar = ({sectionsRef}) => {
-
-  const activeSection = useScrollSpy({
-    sectionElementRefs: sectionsRef,
-    offsetPx:-80,
-    scrollingElement: document.getElementById('main')
-  })
-
+const Navbar = ({sectionsRef, activeSection}) => {
   return (
     <div className='flex h-full items-center md:p-12 p-2 sm:pr-8 '>
       <div className='flex md:block justify-between w-full items-center'>
-        <img className='inline-block md:h-48 h-14 mr-3 rounded-full md:border-8 border-4 border-accent-50/25' src='jonas-cruchon.JPG' />
+        <img className='inline-block md:h-48 h-14 mr-3 rounded-full md:border-8 border-4 border-accent-50/25' src='/assets/img/jonas-cruchon.JPG' />
         <nav className='md:mt-10 overflow-y-auto'>
           <ul className='text-accent-50 text-center flex space-x-3 md:flex-col'>
             <li className={
