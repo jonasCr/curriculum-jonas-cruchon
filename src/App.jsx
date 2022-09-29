af501453b80041fc94c529bc732e64b5482640f5
jonasCr <jonascr.dev@gmail.com>
2022-09-29 12:36:46 +0200

    feat: add scrollspy for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import React, { useRef } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
 
 function App() {
+
+  const sectionsRefs = [useRef(null),useRef(null),useRef(null),useRef(null),useRef(null)]
+
   return (
     <div className='block md:flex h-screen'>
-      <div className="bg-accent-900 text-textAccent flex-none md:h-full">
-        <Navbar></Navbar>
+      <div className="bg-accent-900 text-textAccent flex-none md:h-full w-full md:w-auto">
+        <Navbar sectionsRef={sectionsRefs}></Navbar>
       </div>
-      <div className='flex-auto bg-accent-50'>
-        <Main/>
+      <div id='main' className='flex-auto h-screen overflow-y-scroll bg-accent-50'>
+        <Main sectionsRef={sectionsRefs}/>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
+import useScrollSpy from '../hooks/UseScrollSpy';
 
-const Navbar = () => {
+const Navbar = ({sectionsRef}) => {
+
+  const activeSection = useScrollSpy({
+    sectionElementRefs: sectionsRef,
+    offsetPx:-80,
+    scrollingElement: document.getElementById('main')
+  })
+
   return (
     <div className='flex h-full items-center md:p-12 p-2 sm:pr-8 '>
       <div className='flex md:block justify-between w-full items-center'>
         <img className='inline-block md:h-48 h-14 mr-3 rounded-full md:border-8 border-4 border-accent-50/25' src='jonas-cruchon.JPG' />
         <nav className='md:mt-10 overflow-y-auto'>
           <ul className='text-accent-50 text-center flex space-x-3 md:flex-col'>
-            <li className={`font-bold md:mb-4 text-opacity-60 opacity-50`}>About</li>
-            <li className={`font-bold md:mb-4 text-opacity-60 opacity-50`}>Experience</li>
-            <li className={`font-bold md:mb-4 text-opacity-60`}>Education</li>
-            <li className={`font-bold md:mb-4 text-opacity-60 opacity-50`}>Skills</li>
-            <li className={`font-bold md:mb-4 text-opacity-60 opacity-50`}>Certification</li>
+            <li className={
+                activeSection == 0 
+                ? 'font-bold md:mb-4 text-opacity-60'
+                : 'font-bold md:mb-4 text-opacity-60 opacity-50'
+              }>About</li>
+            <li className={
+                activeSection == 1
+                ? 'font-bold md:mb-4 text-opacity-60'
+                : 'font-bold md:mb-4 text-opacity-60 opacity-50'
+              }>Experience</li>
+            <li className={
+                activeSection == 2
+                ? 'font-bold md:mb-4 text-opacity-60'
+                : 'font-bold md:mb-4 text-opacity-60 opacity-50'
+              }>Education</li>
+            <li className={
+                activeSection == 3 
+                ? 'font-bold md:mb-4 text-opacity-60'
+                : 'font-bold md:mb-4 text-opacity-60 opacity-50'
+              }>Skills</li>
+            <li className={
+                activeSection == 4 
+                ? 'font-bold md:mb-4 text-opacity-60'
+                : 'font-bold md:mb-4 text-opacity-60 opacity-50'
+              }>Certification</li>
           </ul>
         </nav>
       </div>
