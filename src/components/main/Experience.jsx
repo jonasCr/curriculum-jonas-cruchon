cdbcae6f61a8978ecc0bdafd6821b74e0a593f0d
jonasCr <jonascr.dev@gmail.com>
2022-09-30 11:08:42 +0200

    refactor: usiing grid for experience section

--- src/components/main/Experience.jsx
+++ src/components/main/Experience.jsx
@@ -7,16 +7,18 @@ const Experience = () => {
       {
         [0,1,2,3,4,5].map((item,key)=> (
           <div className='md:mb-16 mb-8' key={key}>
-            <div className='md:flex block'>
-              <div className='flex-auto'>
+            <div className='grid grid-cols-3'>
+              <div className='col-span-3 md:col-span-2'>
                 <h3 className='md:text-2xl text-xl text-primary-900 uppercase font-bold'>Desarollador Full Stack</h3>
                 <h4 className='md:text-xl text-lg uppercase font-normal text-primary-700'>Sopra Steria, Madrid, Spain</h4>
-                <h5 className='md:hidden block text-accent-900 font-medium text-sm '>Feb 2018 - Present</h5>
-                <p className='font-light text-primary-700 mt-6'>Lorem ipsum dolor sit</p>
               </div>
-              <div className='flex-none hidden md:block'>
+              <div className='col-span-3 md:col-span-1 md:text-right'>
                 <h5 className='text-accent-900 font-medium text-sm '>Feb 2018 - Present</h5>
               </div>
+              <div className='col-span-3 text-justify'>
+                <p className='font-light text-primary-700 mt-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi pariatur illo quia officia aut? Tempora excepturi voluptas dolores, illum corrupti veniam earum minus voluptatum dolore nemo pariatur ut magni! Deserunt.</p>
+              </div>
+              
             </div>
           </div>
         ))
