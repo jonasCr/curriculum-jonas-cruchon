1e3a30cb950f83c777423e1474adadf607fb426b
jonasCr <jonascr.dev@gmail.com>
2022-09-30 09:44:31 +0200

    feat: add experience section with dummy data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,19 +10,19 @@ const Main = ({sectionsRef}) => {
   
   return (
     <main>
-      <section id="about" ref={sectionsRef[0]} className='h-screen flex items-center border-b-4 border-accent-900'>
+      <section id="about" ref={sectionsRef[0]} className='min-h-screen flex items-center border-b-4 border-accent-900'>
         <About />
       </section>
-      <section id="experience" ref={sectionsRef[1]} className='h-screen flex items-center border-b-4 border-accent-900'>
+      <section id="experience" ref={sectionsRef[1]} className='min-h-screen flex border-b-4 border-accent-900'>
         < Experience />
       </section>
-      <section id="education" ref={sectionsRef[2]} className='h-screen flex items-center border-b-4 border-accent-900'>
+      <section id="education" ref={sectionsRef[2]} className='min-h-screen flex items-center border-b-4 border-accent-900'>
         < Education />
       </section>
-      <section id="skills" ref={sectionsRef[3]} className='h-screen flex items-center border-b-4 border-accent-900'>
+      <section id="skills" ref={sectionsRef[3]} className='min-h-screen flex items-center border-b-4 border-accent-900'>
         < Skills />
       </section>
-      <section id="certification" ref={sectionsRef[4]} className='h-screen flex items-center border-b-4 border-accent-900'>
+      <section id="certification" ref={sectionsRef[4]} className='min-h-screen flex items-center border-b-4 border-accent-900'>
         < Certification />
       </section>
     </main>
